50b86489658189f55f4ad54d917c97c4965ef9a8
Giacomo Pumapillo <giacomo.pumapillo@outlook.it>
2025-02-22 19:37:59 +0100

    update crashes

--- devmate/src/main/java/com/onelife/devmate/DevmateApplication.java
+++ devmate/src/main/java/com/onelife/devmate/DevmateApplication.java
@@ -2,14 +2,12 @@ package com.onelife.devmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class DevmateApiApplication {
+public class DevmateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevmateApiApplication.class, args);
+		SpringApplication.run(DevmateApplication.class, args);
 	}
 
 }
--- devmate/src/main/java/com/onelife/devmate/model/Role.java
+++ devmate/src/main/java/com/onelife/devmate/model/Role.java
@@ -1,41 +1,4 @@
-package com.onelife.devmate.role;
+package com.onelife.devmate.model;
 
-import com.onelife.devmate.user.User;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@Entity
-@Table(name = "user")
-@EntityListeners(AuditingEntityListener.class)
 public class Role {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-    @Column(unique = true)
-    private String name;
-
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users;
-
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
 }
